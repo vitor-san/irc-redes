7fa941afc5b2c278f0e5f18ddfc44836fc367641
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-04-08 01:44:05 -0300

    Added .editorconfig and a basic c++ code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    system("clear");
+    cout << ":vitor-san KICK #party jao-ramos :Xupa Jao!\n";
+
+    while (true) {
+        cout << "HOJE TEM GOL DO RIBAMAAAR\n";
+        sleep(2);
+    }
+
+    return 0;
+}
